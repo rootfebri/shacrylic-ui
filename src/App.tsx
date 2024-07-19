d0fe2e371911fe1cce7c485e55e692c605bbc85b
Febri <root.febri@gmail.com>
2024-07-19 21:41:33 +0700

    Auto commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,51 +4,257 @@ import React from "react";
 import {Label} from "@/components/ui/label.tsx";
 import {Input} from "@/components/ui/input.tsx";
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select.tsx";
+import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@/components/ui/accordion.tsx";
+import {Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger} from "@/components/ui/dialog.tsx";
+import {DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuPortal, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
+import {CalendarDays, Cloud, CreditCard, Github, Keyboard, LifeBuoy, LogOut, Mail, MessageSquare, Plus, PlusCircle, Settings, User, UserPlus, Users} from "lucide-react";
+import {HoverCard, HoverCardContent, HoverCardTrigger} from "@/components/ui/hover-card.tsx";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar.tsx";
 
 type CardProps = React.ComponentProps<typeof Card>
 const App: React.FC<CardProps> = ({className, ...props}) => {
-    return (<div className={'flex flex-row justify-center container py-5 max-h-max'} {...props}>
-        <Card className="w-[350px]">
-            <CardHeader>
-                <CardTitle>Create project</CardTitle>
-                <CardDescription>Deploy your new project in one-click.</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <form>
-                    <div className="grid w-full items-center gap-4">
-                        <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="name">Name</Label>
-                            <Input id="name" placeholder="Name of your project" />
+    return (
+        <div className={'flex flex-row justify-center container py-5 max-h-max space-x-8'} {...props}>
+            <Card className="w-[350px]">
+                <CardHeader>
+                    <CardTitle>Create project</CardTitle>
+                    <CardDescription>Deploy your new project in one-click.</CardDescription>
+                </CardHeader>
+                <CardContent className={'space-y-8'}>
+                    <Accordion type="single" collapsible className="w-full">
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                            <AccordionContent>
+                                Yes. It adheres to the WAI-ARIA design pattern.
+                            </AccordionContent>
+                        </AccordionItem>
+                        <AccordionItem value="item-2">
+                            <AccordionTrigger>Is it styled?</AccordionTrigger>
+                            <AccordionContent>
+                                Yes. It comes with default styles that matches the other
+                                components&apos; aesthetic.
+                            </AccordionContent>
+                        </AccordionItem>
+                        <AccordionItem value="item-3">
+                            <AccordionTrigger>Is it animated?</AccordionTrigger>
+                            <AccordionContent>
+                                Yes. It&apos;s animated by default, but you can disable it if you
+                                prefer.
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+
+                    <form>
+                        <div className="grid w-full items-center gap-4">
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="name">Name</Label>
+                                <Input id="name" placeholder="Name of your project"/>
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="framework">Framework</Label>
+                                <Select>
+                                    <SelectTrigger id="framework">
+                                        <SelectValue placeholder="Select"/>
+                                    </SelectTrigger>
+                                    <SelectContent position="popper">
+                                        <SelectItem value="next">Next.js</SelectItem>
+                                        <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                                        <SelectItem value="astro">Astro</SelectItem>
+                                        <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </div>
                         </div>
-                        <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="framework">Framework</Label>
-                            <Select>
-                                <SelectTrigger id="framework">
-                                    <SelectValue placeholder="Select" />
-                                </SelectTrigger>
-                                <SelectContent position="popper">
-                                    <SelectItem value="next">Next.js</SelectItem>
-                                    <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                                    <SelectItem value="astro">Astro</SelectItem>
-                                    <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                                </SelectContent>
-                            </Select>
+                    </form>
+                </CardContent>
+
+                <CardFooter className="flex justify-center space-y-2">
+                    <div className={'flex flex-col justify-center items-center space-y-2 max-w-fit'}>
+                        <Button>Deploy</Button>
+                        <Button variant="secondary">secondary</Button>
+                        <Button variant="outline">outline</Button>
+                        <Button variant="destructive">destructive</Button>
+                        <Button variant="ghost">ghost</Button>
+                        <Button variant="link">https://google.com</Button>
+                    </div>
+                </CardFooter>
+            </Card>
+
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline">Open</Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuGroup>
+                        <DropdownMenuItem>
+                            <User className="mr-2 h-4 w-4" />
+                            <span>Profile</span>
+                            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <CreditCard className="mr-2 h-4 w-4" />
+                            <span>Billing</span>
+                            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Settings className="mr-2 h-4 w-4" />
+                            <span>Settings</span>
+                            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Keyboard className="mr-2 h-4 w-4" />
+                            <span>Keyboard shortcuts</span>
+                            <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuGroup>
+                        <DropdownMenuItem>
+                            <Users className="mr-2 h-4 w-4" />
+                            <span>Team</span>
+                        </DropdownMenuItem>
+                        <DropdownMenuSub>
+                            <DropdownMenuSubTrigger>
+                                <UserPlus className="mr-2 h-4 w-4" />
+                                <span>Invite users</span>
+                            </DropdownMenuSubTrigger>
+                            <DropdownMenuPortal>
+                                <DropdownMenuSubContent>
+                                    <DropdownMenuItem>
+                                        <Mail className="mr-2 h-4 w-4" />
+                                        <span>Email</span>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem>
+                                        <MessageSquare className="mr-2 h-4 w-4" />
+                                        <span>Message</span>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuSeparator />
+                                    <DropdownMenuItem>
+                                        <PlusCircle className="mr-2 h-4 w-4" />
+                                        <span>More...</span>
+                                    </DropdownMenuItem>
+                                </DropdownMenuSubContent>
+                            </DropdownMenuPortal>
+                        </DropdownMenuSub>
+                        <DropdownMenuItem>
+                            <Plus className="mr-2 h-4 w-4" />
+                            <span>New Team</span>
+                            <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <Github className="mr-2 h-4 w-4" />
+                        <span>GitHub</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <LifeBuoy className="mr-2 h-4 w-4" />
+                        <span>Support</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem disabled>
+                        <Cloud className="mr-2 h-4 w-4" />
+                        <span>API</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <LogOut className="mr-2 h-4 w-4" />
+                        <span>Log out</span>
+                        <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+
+            <HoverCard>
+                <HoverCardTrigger asChild>
+                    <Button variant="link">@nextjs</Button>
+                </HoverCardTrigger>
+                <HoverCardContent className="w-80">
+                    <div className="flex justify-between space-x-4">
+                        <Avatar>
+                            <AvatarImage src="https://github.com/vercel.png" />
+                            <AvatarFallback>VC</AvatarFallback>
+                        </Avatar>
+                        <div className="space-y-1">
+                            <h4 className="text-sm font-semibold">@nextjs</h4>
+                            <p className="text-sm">
+                                The React Framework – created and maintained by @vercel.
+                            </p>
+                            <div className="flex items-center pt-2">
+                                <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+                                <span className="text-xs text-muted-foreground">Joined December 2021</span>
+                            </div>
                         </div>
                     </div>
-                </form>
-            </CardContent>
-            <CardFooter className="flex justify-center space-y-2">
-                <div className={'flex flex-col space-y-2'}>
-                    <Button>Deploy</Button>
-                    <Button variant="secondary">secondary</Button>
-                    <Button variant="outline">outline</Button>
-                    <Button variant="destructive">destructive</Button>
-                    <Button variant="ghost">ghost</Button>
-                    <Button variant="link">google.com</Button>
-                </div>
-            </CardFooter>
-        </Card>
-    </div>)
+                </HoverCardContent>
+            </HoverCard>
+
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button variant="outline">Into Dialog</Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Dialog Title</DialogTitle>
+                        <DialogDescription>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, atque beatae blanditiis delectus eos facilis fuga in laudantium nemo obcaecati optio, placeat quaerat, quas quo sequi sunt tenetur veniam voluptatum.
+                        </DialogDescription>
+                    </DialogHeader>
+
+                    <Accordion type="single" collapsible className="w-full">
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                            <AccordionContent>
+                                Yes. It adheres to the WAI-ARIA design pattern.
+                            </AccordionContent>
+                        </AccordionItem>
+                        <AccordionItem value="item-2">
+                            <AccordionTrigger>Is it styled?</AccordionTrigger>
+                            <AccordionContent>
+                                Yes. It comes with default styles that matches the other
+                                components&apos; aesthetic.
+                            </AccordionContent>
+                        </AccordionItem>
+                        <AccordionItem value="item-3">
+                            <AccordionTrigger>Is it animated?</AccordionTrigger>
+                            <AccordionContent>
+                                Yes. It&apos;s animated by default, but you can disable it if you
+                                prefer.
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+
+                    <form>
+                        <div className="grid w-full items-center gap-4">
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="name">Name</Label>
+                                <Input id="name" placeholder="Name of your project"/>
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="framework">Framework</Label>
+                                <Select>
+                                    <SelectTrigger id="framework">
+                                        <SelectValue placeholder="Select"/>
+                                    </SelectTrigger>
+                                    <SelectContent position="popper">
+                                        <SelectItem value="next">Next.js</SelectItem>
+                                        <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                                        <SelectItem value="astro">Astro</SelectItem>
+                                        <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                        </div>
+                    </form>
+
+                    <DialogFooter>
+                        <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </div>
+    )
 }
 
 export default App
--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -12,7 +12,7 @@ const SelectValue = SelectPrimitive.Value
 
 const SelectTrigger = React.forwardRef<React.ElementRef<typeof SelectPrimitive.Trigger>, React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>>(({className, children, ...props}, ref) => (<SelectPrimitive.Trigger
     ref={ref}
-    className={cn("acrylic flex h-10 w-full items-center justify-between rounded-md border border-input/65 bg-background/5 px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-0 focus:ring-ring focus:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1", className)}
+    className={cn("flex h-10 w-full items-center justify-between rounded-md border border-input/65 bg-background/50 px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-0 focus:ring-ring focus:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1", className)}
     {...props}
 >
     {children}
